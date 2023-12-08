922906a2e51b62508bafc179d02d73bc3319cdd9
Tolash123 <idreesy31@gmail.com>
2023-12-08 19:08:24 +0100

    day17

--- Day17/day17.py
+++ Day17/day17.py
@@ -1,4 +1,4 @@
-# To printsquare pattern with * symbols
+# To print square pattern with * symbols
 n = int(input('Enter no of rows: '))
 for i in range(n):
     print('* '*n)
@@ -19,3 +19,25 @@ for i in range(n):
     for j in range(i+1):
         print('+', end=' ')
     print()
+
+# Print Inverted Right Angle Triangle pattern with $ symbol
+n = int(input('Enter No of rows: '))
+for i in range(n):
+    print('$ '*(n-i))
+
+# To Print Pyramid pattern with + symbols
+n = int(input('Enter No of rows: '))
+for i in range(n):
+    print(' '*(n-i-1)+(' +')*(i+1))
+
+# Print Inverted Pyramid Pattern with + symbols
+n = int(input('Enter No of rows: '))
+for i in range(n):
+    print(' '*i+' +'*(n-i))
+
+#  Print Diamond Pattern with * symbols
+n = int(input('Enter No of rows: '))
+for i in range(n):
+    print(' '*(n-i-1)+(' +')*(i+1))
+for i in range(n):
+    print(' '*i+' +'*(n-i))
