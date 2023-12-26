13c988afcd3d919a552cf71b41815c9b0676097e
Tolash123 <idreesy31@gmail.com>
2023-12-26 20:57:14 +0100

    updated

--- Day20/projects/8.py
+++ Day20/projects/8.py
@@ -11,3 +11,9 @@ print("\033[0m", end="") #default
 print("get odd gaps")
 print("\033[36m", end="")
 print('The world of dristol')
+print("If you put", "\033[33m", "nothing as the", "\033[35m", "end character", "\033[32m", "then you don't", "\033[0m", "get odd gaps", end="")
+print()
+# color changing with sep=''
+print("If you put ", "\033[33m", "nothing as the ", "\033[35m", "end character ", "\033[32m", "then you don't ", "\033[0m", "get odd gaps ", sep="")
+
+
