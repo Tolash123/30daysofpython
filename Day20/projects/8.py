9c9d90a198c156082de85efb514784246f4588c5
Tolash123 <idreesy31@gmail.com>
2023-12-26 20:29:43 +0100

    updated

---
+++ Day20/projects/8.py
@@ -0,0 +1,13 @@
+# printing colours
+
+print("If you put")
+print("\033[33m", end="") #yellow
+print("nothing as the")
+print("\033[35m", end="") #purple
+print("end character")
+print("\033[32m", end="") #green
+print("then you don't")
+print("\033[0m", end="") #default
+print("get odd gaps")
+print("\033[36m", end="")
+print('The world of dristol')
