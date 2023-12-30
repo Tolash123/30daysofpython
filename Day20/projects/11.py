bbebdeab60bd010ff53796e71cf82e404a071183
Tolash123 <idreesy31@gmail.com>
2023-12-30 11:27:14 +0100

    user interface string manipulation

---
+++ Day20/projects/11.py
@@ -0,0 +1,97 @@
+#Create a program that uses a loop that asks the user what they have thought of each of the 30 days of challenges so far.
+# Questionnaire
+
+for i in range(1,31):
+    print('Day', i, 'of 30days')
+    if i == 1:
+        day1 = input('What is your thought on day1: ')
+        print('You thought day1 was: ', day1)
+    elif i == 2:
+        day2 = input('What is your thought on day2: ')
+        print('You thought day2 was: ', day2)
+    elif i == 3:
+        day3 = input('What is your thought on day3: ')
+        print('You thought day3 was: ', day3)
+    elif i == 4:
+        day4 = input('What is your thought on day4: ')
+        print('You thought day4 was: ', day4)
+    elif i == 5:
+        day5 = input('What is your thought on day5: ')
+        print('You thought day5 was: ', day5)
+    elif i == 6:
+        day6 = input('What is your thought on day6: ')
+        print('You thought day6 was: ', day6)
+    elif i == 7:
+        day7 = input('What is your thought on day7: ')
+        print('You thought day7 was: ', day7)
+    elif i == 8:
+        day8 = input('What is your thought on day8: ')
+        print('You thought day8 was: ', day8)
+    elif i == 9:
+        day9 = input('What is your thought on day9: ')
+        print('You thought day9 was: ', day9)
+    elif i == 10:
+        day10 = input('What is your thought on day10: ')
+        print('You thought day10 was: ', day10)
+    elif i == 11:
+        day11 = input('What is your thought on day11: ')
+        print('You thought day11 was: ', day11)
+    elif i == 12:
+        day12 = input('What is your thought on day12: ')
+        print('You thought day12 was: ', day12)
+    elif i == 13:
+        day13 = input('What is your thought on day13: ')
+        print('You thought day13 was: ', day13)
+    elif i == 14:
+        day14 = input('What is your thought on day14: ')
+        print('You thought day14 was: ', day14)
+    elif i == 15:
+        day15 = input('What is your thought on day15: ')
+        print('You thought day15 was: ', day15)
+    elif i == 16:
+        day16 = input('What is your thought on day16: ')
+        print('You thought day16 was: ', day16)
+    elif i == 17:
+        day17 = input('What is your thought on day17: ')
+        print('You thought day17 was: ', day17)
+    elif i == 18:
+        day18 = input('What is your thought on day18: ')
+        print('You thought day18 was: ', day18)
+    elif i == 19:
+        day19 = input('What is your thought on day19: ')
+        print('You thought day19 was: ', day19)
+    elif i == 20:
+        day20 = input('What is your thought on day20: ')
+        print('You thought day20 was: ', day20)
+    elif i == 21:
+        day21 = input('What is your thought on day21: ')
+        print('You thought day21 was: ', day21)
+    elif i == 22:
+        day22 = input('What is your thought on day22: ')
+        print('You thought day22 was: ', day22)
+    elif i == 23:
+        day23 = input('What is your thought on day23: ')
+        print('You thought day23 was: ', day23)
+    elif i == 24:
+        day24 = input('What is your thought on day24: ')
+        print('You thought day12 was: ', day24)
+    elif i == 25:
+        day25 = input('What is your thought on day25: ')
+        print('You thought day25 was: ', day25)
+    elif i == 26:
+        day26 = input('What is your thought on day26: ')
+        print('You thought day26 was: ', day26)
+    elif i == 27:
+        day27 = input('What is your thought on day27: ')
+        print('You thought day27 was: ', day27)
+    elif i == 28:
+        day28 = input('What is your thought on day28: ')
+        print('You thought day28 was: ', day28)
+    elif i == 29:
+        day29 = input('What is your thought on day29: ')
+        print('You thought day29 was: ', day29)
+    elif i == 30:
+        day30 = input('What is your thought on day30: ')
+        print('You thought day30 was: ', day30)
+
+print('We really appreciate your feedback \nHappy Coding!')
---
+++ Day20/projects/12.py
@@ -0,0 +1,28 @@
+import os,time
+yellow = '\033[33m'
+green = '\033[32m'
+red = '\033[31m'
+blue = '\033[34m'
+pink = '\033[35m'
+white = '\033[37m'
+print()
+print(yellow,"""    === Music App ===  """, end='')
+print('\n')
+print(white,'Radio Gaga', end='\n')
+print(yellow, 'Queen', end='\n')
+print()
+print(white,'PREV', end='\n')
+print(green, '\tNEXT', end='\n')
+print(red, '\t PAUSE', end='')
+print()
+time.sleep(5)
+os.system('clear')
+
+print(white, '\tWelcome To', end='\n')
+print(blue,"""    --   ARMBOOK  --  """, end='\n')
+print('\n')
+print(yellow, """Definitely not a rip off of a certain other social networking site.""")
+print('\n')
+print(red, '\tHonest.', end='\n')
+print(white, '\tUsername: ', end='\n')
+print(white, '\tPassword: ', end='\n')
